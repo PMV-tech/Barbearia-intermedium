35dd1d27641b067098556274dc327f18dd99ebc3
Kauan Nascimento Ferreira <155115702+KauanNFerreira@users.noreply.github.com>
2026-03-11 20:18:10 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,3 @@
 // Configuração do cliente Supabase
 const SUPABASE_URL = 'https://wshpivbenwjmphfsxayu.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzaHBpdmJlbndqbXBoZnN4YXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjYxMjcsImV4cCI6MjA4ODg0MjEyN30.7qcwkYI3ql9rIiBa5j_0VmpEiAzmdAZPCHa5-jwMOvY'; // Substitua pela sua chave
-
-// Cria o cliente Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
